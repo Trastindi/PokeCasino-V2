e0a1775cb653ae03e885773397f2199a2a9047b0
Silver <alejandro.plata@outlook.es>
2026-06-01 18:02:26 +0200

    Create CalleVictoriaViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/CalleVictoriaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Lugares
+{
+    public class CalleVictoriaViewModel : GachaViewModel
+    {
+        public CalleVictoriaView(User usuario)
+            : base(usuario, "Calle Victoria")
+        {
+        }
+    }
+}
