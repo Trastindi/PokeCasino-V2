c452f6e34b9cb9ecfa5497fb7fbec66e3d0da425
Silver <alejandro.plata@outlook.es>
2026-06-01 17:59:39 +0200

    Create ZonaSafariView.xaml.cs

---
+++ PK-Proyect - Web App/PK-Proyect/View/Banners/Lugares/ZonaSafariView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using PK_Proyect.Models;
+using PK_Proyect.ViewModels.Banners;
+using PK_Proyect.ViewModels.Banners.Lugares;
+using System.Windows;
+namespace PK_Proyect.View.Banners.Lugares
+{
+ 
+    public partial class ZonaSafariView : Window
+    {
+        public ZonaSafariView(User usuario)
+        {
+            InitializeComponent();
+            DataContext = new ZonaSafariViewModel(usuario);
+        }
+    }
+}
