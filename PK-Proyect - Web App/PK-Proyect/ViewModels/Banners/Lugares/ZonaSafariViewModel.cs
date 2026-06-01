c76ff9bfe5bcc450623ddf318a1f31e4f3b60867
Silver <alejandro.plata@outlook.es>
2026-06-01 17:58:51 +0200

    Create ZonaSafariViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/ZonaSafariViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Lugares
+{
+    public class ZonaSafariViewModel : GachaViewModel
+    {
+        public ZonaSafariView(User usuario)
+            : base(usuario, "Zona Safari")
+        {
+        }
+    }
+}
