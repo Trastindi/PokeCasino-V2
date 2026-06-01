5edd3baca273e36fd40e2c557b76fd1740a9425b
Silver <alejandro.plata@outlook.es>
2026-06-01 17:55:21 +0200

    Create MansionPokemonViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/MansionPokemonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Lugares
+{
+    public class MansionPokemonViewModel : GachaViewModel
+    {
+        public MansionPokemonView(User usuario)
+            : base(usuario, "Mansión Pokémon")
+        {
+        }
+    }
+}
