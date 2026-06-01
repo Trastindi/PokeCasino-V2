ec02ae368cd64d50f0a78334685ebf6cff9b218e
Silver <alejandro.plata@outlook.es>
2026-06-01 17:48:57 +0200

    Add TorrePokemonViewModel for Torre Pokemon banner

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/TorrePokemonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Lugares
+{
+    public class TorrePokemonViewModel : GachaViewModel
+    {
+        public TorrePokemonView(User usuario)
+            : base(usuario, "Torre Pokemon")
+        {
+        }
+    }
+}
