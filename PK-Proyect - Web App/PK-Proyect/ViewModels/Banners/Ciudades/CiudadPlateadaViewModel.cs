abdf986a93410743fc3e86bbd2c7e1b7d761a72c
Silver <alejandro.plata@outlook.es>
2026-06-01 09:45:22 +0200

    Add CiudadPlateadaViewModel class for Ciudad Plateada
    Se añade nueva ciudad en el ViewsModel

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Ciudades/CiudadPlateadaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Ciudades
+{
+    public class CiudadPlateadaViewModel : GachaViewModel
+    {
+        public CiudadPlateadaViewModel(User usuario)
+            : base(usuario, "Ciudad Plateada")
+        {
+        }
+    }
+}
