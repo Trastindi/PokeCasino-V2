9a504d86a3f67eb158bf137ddfc0c3c033b66024
Silver <alejandro.plata@outlook.es>
2026-06-01 09:49:08 +0200

    Add CiudadAzafranViewModel class
    Añadida Ciudad Azafran ViewModel

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Ciudades/CiudadAzafranViewModel.xaml.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Ciudades
+{
+    public class CiudadAzafranViewModel : GachaViewModel
+    {
+        public CiudadAzafranViewModel(User usuario)
+            : base(usuario, "Ciudad Azafran")
+        {
+        }
+    }
+}
