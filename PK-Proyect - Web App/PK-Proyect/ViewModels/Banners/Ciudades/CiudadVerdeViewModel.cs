a270acd75b9716c2477dac168601894fc78fdc5a
Silver <alejandro.plata@outlook.es>
2026-06-01 09:09:09 +0200

    Add CiudadVerdeViewModel for Ciudad Verde banner
    Se añade el ViewModel banner/ciudad de Ciudad Verde

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Ciudades/CiudadVerdeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Ciudades
+{
+    public class CiudadVerdeViewModel : GachaViewModel
+    {
+        public CiudadVerdeViewModel(User usuario)
+            : base(usuario, "Ciudad Verde")
+        {
+        }
+    }
+}
