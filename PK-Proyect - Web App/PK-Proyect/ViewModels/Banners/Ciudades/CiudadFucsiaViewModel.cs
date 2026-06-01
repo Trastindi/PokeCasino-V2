6820484d1ea5d7a2f7bf5af25dc4efbc22a6c7ee
Silver <alejandro.plata@outlook.es>
2026-06-01 09:55:44 +0200

    Create CiudadFucsiaViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Ciudades/CiudadFucsiaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Ciudades
+{
+    public class CiudadFucsiaViewModel : GachaViewModel
+    {
+        public CiudadFucsiaViewModel(User usuario)
+            : base(usuario, "Ciudad Fucsia")
+        {
+        }
+    }
+}
