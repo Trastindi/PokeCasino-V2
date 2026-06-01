68a07ead687dd142803e42443a7f4e3677c565f0
Silver <alejandro.plata@outlook.es>
2026-06-01 09:56:09 +0200

    Add CiudadFucsiaView class for city banner

---
+++ PK-Proyect - Web App/PK-Proyect/View/Banners/Ciudades/CiudadFucsiaView.cs
@@ -0,0 +1,15 @@
+﻿using PK_Proyect.ViewModels.Banners.Ciudades;
+using System.Windows;
+using PK_Proyect.Models;
+
+namespace PK_Proyect.View.Banners.Ciudades
+{
+    public partial class CiudadFucsiaView : Window
+    {
+        public CiudadFucsiaView(User usuario)
+        {
+            InitializeComponent();
+            DataContext = new CiudadFucsiaViewModel(usuario);
+        }
+    }
+}
