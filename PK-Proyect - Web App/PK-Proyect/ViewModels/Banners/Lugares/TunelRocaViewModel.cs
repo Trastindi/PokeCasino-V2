72cdfe64b3e86ee56ee6c85fa5905bbe3675e88d
Silver <alejandro.plata@outlook.es>
2026-06-01 10:23:22 +0200

    Create TunelRocaViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/TunelRocaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Lugares
+{
+    public class TunelRocaViewModel : GachaViewModel
+    {
+        public TunelRocaViewModel(User usuario)
+            : base(usuario, "Tunel Roca")
+        {
+        }
+    }
+}
