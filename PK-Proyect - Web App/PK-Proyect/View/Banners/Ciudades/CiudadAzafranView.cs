9f1a2a0cd97f73db85711336121d1e90320ce69c
Silver <alejandro.plata@outlook.es>
2026-06-01 09:50:07 +0200

    Create CiudadAzafranView.cs
    Añadido Ciudad Azafran View

---
+++ PK-Proyect - Web App/PK-Proyect/View/Banners/Ciudades/CiudadAzafranView.cs
@@ -0,0 +1,15 @@
+﻿using PK_Proyect.ViewModels.Banners.Ciudades;
+using System.Windows;
+using PK_Proyect.Models;
+
+namespace PK_Proyect.View.Banners.Ciudades
+{
+    public partial class CiudadAzafranView : Window
+    {
+        public CiudadAzafranView(User usuario)
+        {
+            InitializeComponent();
+            DataContext = new CiudadAzafranViewModel(usuario);
+        }
+    }
+}
