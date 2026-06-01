38b7bf2ae24efba12c756271231fcd94f48bf3a5
Silver <alejandro.plata@outlook.es>
2026-06-01 09:09:26 +0200

    Add CiudadVerdeView with ViewModel initialization
    Se añade el View banner/ciudad de Ciudad Verde

---
+++ PK-Proyect - Web App/PK-Proyect/View/Banners/Ciudades/CiudadVerdeViewModel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PK_Proyect.ViewModels.Banners.Ciudades;
+using System.Windows;
+using PK_Proyect.Models;
+
+namespace PK_Proyect.View.Banners.Ciudades
+{
+    public partial class CiudadVerdeView : Window
+    {
+        public CiudadVerdeView(User usuario)
+        {
+            InitializeComponent();
+            DataContext = new CiudadVerdeViewModel(usuario);
+        }
+    }
+}
