98f75ff24da890f1b58a02333b261e6e4ca50a0f
Silver <alejandro.plata@outlook.es>
2026-06-02 10:27:30 +0200

    Create MesetaAnilViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/MesetaAnilViewModel.cs
@@ -0,0 +1,12 @@
+using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Rutas
+{
+    public class MesetaAnilViewModel : GachaViewModel
+    {
+        public MesetaAnilViewModel(User usuario)
+            : base(usuario, "Meseta Añil")
+        {
+        }
+    }
+}
