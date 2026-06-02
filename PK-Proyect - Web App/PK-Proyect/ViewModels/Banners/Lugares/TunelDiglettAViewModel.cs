4ff0c04eaece532991c8799a9c5aa973824e5b38
Silver <alejandro.plata@outlook.es>
2026-06-02 10:32:49 +0200

    Create TunelDiglettAViewModel.cs

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/TunelDiglettAViewModel.cs
@@ -0,0 +1,12 @@
+using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Rutas
+{
+    public class TunelDiglettAViewModel : GachaViewModel
+    {
+        public TunelDiglettAViewModel(User usuario)
+            : base(usuario, "Tunel Diglett")
+        {
+        }
+    }
+}
