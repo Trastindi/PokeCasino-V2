a0e593384a66cae4063262365364e09cee20c823
Silver <alejandro.plata@outlook.es>
2026-06-02 18:11:10 +0200

    fix: Cargar equipo pokemon de forma asincrónica para no bloquear UI

--- PK-Proyect - Web App/PK-Proyect/ViewModels/EquipoPokemonViewModel.cs
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/EquipoPokemonViewModel.cs
@@ -1,6 +1,8 @@
-﻿using PK_Proyect.Models;
+using PK_Proyect.Models;
 using PK_Proyect.Repositories;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PK_Proyect.ViewModels
 {
@@ -15,26 +17,23 @@ namespace PK_Proyect.ViewModels
             _repo = new PokemonUserRepository();
             Equipo = new ObservableCollection<PokemonUser>();
 
-            CargarEquipo(userId);
+            // Cargar de forma asincrónica para no bloquear el UI
+            _ = CargarEquipoAsync(userId);
         }
 
-        private void CargarEquipo(string userId)
+        private async Task CargarEquipoAsync(string userId)
         {
-            Equipo.Clear();
-
-            var lista = _repo.GetPokemonsByUser(userId)
-                 .OrderBy(p => p.PokemonId)
-                 .ToList();
-
+            var lista = await Task.Run(() =>
+                _repo.GetPokemonsByUser(userId)
+                    .OrderBy(p => p.PokemonId)
+                    .ToList()
+            );
 
             foreach (var p in lista)
             {
                 Console.WriteLine($"Pokemon: {p.Nombre}, Nivel: {p.Nivel}, Cantidad: {p.Cantidad}");
                 Equipo.Add(p);
             }
-
-
-
         }
     }
 }
