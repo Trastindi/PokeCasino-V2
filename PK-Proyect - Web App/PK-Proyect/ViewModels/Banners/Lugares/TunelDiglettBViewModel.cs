4beb1b39f74920bf4769b2e0f24c62c67f3040eb
Silver <alejandro.plata@outlook.es>
2026-06-02 10:34:13 +0200

    Add TunelDiglettBViewModel class for Gacha functionality

---
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/Banners/Lugares/TunelDiglettBViewModel.cs
@@ -0,0 +1,12 @@
+using PK_Proyect.Models;
+
+namespace PK_Proyect.ViewModels.Banners.Rutas
+{
+    public class TunelDiglettBViewModel : GachaViewModel
+    {
+        public TunelDiglettBViewModel(User usuario)
+            : base(usuario, "Tunel Diglett")
+        {
+        }
+    }
+}
