7f5e5d0276301da1922692ebc28ddd03e4c880c8
Alejandro Plata González <alejandro.plata@outlook.es>
2026-06-15 13:18:22 +0200

    Añadidos los View del battlw window y del search
    Se añaden los Views correspondientes al BattleWindow y al SearchBattle pendientes de trermina r de configurar

---
+++ PK-Proyect - Web App/PK-Proyect/View/BattleWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using PK_Proyect.Services;
+using PK_Proyect.ViewModels;
+
+namespace PK_Proyect.View
+{
+    public partial class BattleWindowView : Window
+    {
+        public BattleWindowView(IBattleService battleService)
+        {
+            InitializeComponent();
+            DataContext = new BattleWindowViewModel(battleService);
+        }
+    }
+}
---
+++ PK-Proyect - Web App/PK-Proyect/View/SearchBattleView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using PK_Proyect.Services;
+using PK_Proyect.ViewModels;
+
+namespace PK_Proyect.View
+{
+    public partial class SearchBattle : Window
+    {
+        private readonly SearchBattleViewModel _vm;
+
+        public SearchBattle(IBattleService battleService)
+        {
+            InitializeComponent();
+            _vm = new SearchBattleViewModel(battleService, Close);
+            _vm.BattleAccepted += OnBattleAccepted;
+            DataContext = _vm;
+        }
+
+        private void OnBattleAccepted()
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                var battleWindow = new BattleWindow();
+                battleWindow.Owner = this.Owner;
+                battleWindow.Show();
+                this.Close();
+            });
+        }
+    }
+}
