2e1640b49d140c1d7e2b364509bebbdc4328027e
Trastindi <116264219+Trastindi@users.noreply.github.com>
2026-06-18 00:51:48 +0200

    fix: cargar lista de pokemon en hilo background para evitar congelado UI

--- PK-Proyect - Web App/PK-Proyect/ViewModels/PokemonObtenidosViewModel.cs
+++ PK-Proyect - Web App/PK-Proyect/ViewModels/PokemonObtenidosViewModel.cs
@@ -2,33 +2,43 @@
 using PK_Proyect.Repositories;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace PK_Proyect.ViewModels
 {
     public class PokemonObtenidosViewModel
     {
         private readonly PokemonUserRepository _repo;
+        private readonly string _userId;
 
         public ObservableCollection<PokemonUser> Lista { get; set; }
 
         public PokemonObtenidosViewModel(string userId)
         {
-            _repo = new PokemonUserRepository();
-            Lista = new ObservableCollection<PokemonUser>();
+            _repo   = new PokemonUserRepository();
+            _userId = userId;
+            Lista   = new ObservableCollection<PokemonUser>();
 
-            Cargar(userId);
+            // Carga en background para no bloquear el hilo UI
+            _ = CargarAsync();
         }
 
-        private void Cargar(string userId)
+        private async Task CargarAsync()
         {
-            Lista.Clear();
+            var pokes = await Task.Run(() =>
+                _repo.GetPokemonsByUser(_userId)
+                     .OrderByDescending(p => p.FechaObtenido)
+                     .ToList()
+            );
 
-            var pokes = _repo.GetPokemonsByUser(userId)
-                             .OrderByDescending(p => p.FechaObtenido)
-                             .ToList();
-
-            foreach (var p in pokes)
-                Lista.Add(p);
+            // Volver al hilo UI para modificar la ObservableCollection
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Lista.Clear();
+                foreach (var p in pokes)
+                    Lista.Add(p);
+            });
         }
     }
 }
