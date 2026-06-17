39ff4853d5bd3e5ad722efd8d3adfd77e7875e85
Alejandro Plata González <alejandro.plata@outlook.es>
2026-06-17 16:17:21 +0200

    Create MenuPokemonView.xaml.cs

---
+++ PK-Proyect - Web App/PK-Proyect/View/MenuPokemonView.xaml.cs
@@ -0,0 +1,37 @@
+using PK_Proyect.Models;
+using PK_Proyect.Services;
+using PK_Proyect.ViewModels;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PK_Proyect.View
+{
+    public partial class MenuPokemonView : Window
+    {
+        private readonly MenuPokemonViewModel _vm;
+
+        public MenuPokemonView(User usuario, UserService userService)
+        {
+            InitializeComponent();
+            _vm = new MenuPokemonViewModel(usuario, userService);
+            DataContext = _vm;
+            _vm.CerrarSesionRequested += OnCerrarSesion;
+        }
+
+        private void OnCerrarSesion()
+        {
+            new LoginView().Show();
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+        private void btnClose_Click(object sender, RoutedEventArgs e) => Close();
+
+       
+    }
+}
