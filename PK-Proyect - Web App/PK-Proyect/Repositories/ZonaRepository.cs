95cfe9f853a0403322949f68ff892ea735c4c4c9
Trastindi <116264219+Trastindi@users.noreply.github.com>
2026-06-18 12:54:57 +0200

    Refactor ZonaRepository to use ApiClient for data access

--- PK-Proyect - Web App/PK-Proyect/Repositories/ZonaRepository.cs
+++ PK-Proyect - Web App/PK-Proyect/Repositories/ZonaRepository.cs
@@ -1,4 +1,3 @@
-using MongoDB.Driver;
 using PK_Proyect.Models;
 using System.Collections.Generic;
 
@@ -6,21 +5,19 @@ namespace PK_Proyect.Repositories
 {
     public class ZonaRepository : IZonaRepository
     {
-        private readonly IMongoCollection<Zona> _zonas;
-
-        public ZonaRepository()
-        {
-            _zonas = MongoDbContext.GetCollection<Zona>("Zonas");
-        }
-
         public List<Zona> ObtenerTodas()
-        {
-            return _zonas.Find(z => true).ToList();
-        }
+            => ApiClient.Get<List<Zona>>("/zonas");
 
         public Zona ObtenerPorNombre(string nombre)
         {
-            return _zonas.Find(z => z.Nombre == nombre).FirstOrDefault();
+            try
+            {
+                return ApiClient.Get<Zona>($"/zonas/{nombre}");
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
