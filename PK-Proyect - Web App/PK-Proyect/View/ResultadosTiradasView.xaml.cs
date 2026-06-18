aefbbbae1c8cae677e45d3508e48ee8979f799a5
Alejandro Plata González <alejandro.plata@outlook.es>
2026-06-18 16:48:03 +0200

    Add files via upload

---
+++ PK-Proyect - Web App/PK-Proyect/View/ResultadosTiradasView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using PK_Proyect.ViewModels;
+
+namespace PK_Proyect.Views
+{
+    public partial class ResultadoTiradasView : Window
+    {
+        public ResultadoTiradasView(ResultadoTiradasViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void Cerrar(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
