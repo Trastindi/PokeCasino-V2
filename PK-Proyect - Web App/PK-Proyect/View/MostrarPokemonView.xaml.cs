299c0a297b9841cbb58ea81560d7d47e6f6a9daf
Alejandro Plata González <alejandro.plata@outlook.es>
2026-06-18 16:22:59 +0200

    Add files via upload

---
+++ PK-Proyect - Web App/PK-Proyect/View/MostrarPokemonView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using PK_Proyect.ViewModels;
+
+namespace PK_Proyect.Views
+{
+    public partial class MostrarPokemonView : Window
+    {
+        public MostrarPokemonView(MostrarPokemonViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void Cerrar(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
